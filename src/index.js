6a35039581bfd27a9061a5855cd068ac21edb124
Nataliia Krikun <58227698+Natika888@users.noreply.github.com>
2020-10-20 13:56:16 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -34,12 +34,33 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+    '-----':  '0',    
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];  
+  let array = expr.split('');
+  for (let i = 0; i<array.length; i+=10) {
+    arr.push(expr.slice(i,i+10));
+  }
+
+  let result = arr.map( function (el) {
+    if (el === '**********') {return ' '}
+    else {
+      let arrNew = [];  
+      let arrayNew = el.split('');
+      for (let i = 0; i<arrayNew.length; i+=2) {
+        console.log(el.slice(i,i+2));
+        if (el.slice(i,i+2) === '00') {continue}
+          else if (el.slice(i,i+2) === '10') {arrNew.push('.')}
+               else if (el.slice(i,i+2) === '11') {arrNew.push('-')}
+      } return MORSE_TABLE[arrNew.join('')];              
+    }
+  });
+
+  return result.join('');
 }
+
 
 module.exports = {
     decode
